c7bf3dd9c9bec63aad38cba3b00ceceafac7799d
Joel <joelvb1993@gmail.com>
2024-10-02 15:59:10 -0600

    apuntes

---
+++ My-app/Apuntes.js
@@ -0,0 +1,37 @@
+/*
+version: '3'
+
+services:
+  mongo:
+    image: mongo
+    restart: always
+    environment:
+     - MONGO_INITDB_ROOT_USERNAME: joel
+     - MONGO_INITDB_ROOT_PASSWORD: password
+
+
+hello: 
+  build:
+    context: .
+  volumes:
+    - ./pages/api: /hello
+  ports:
+    - '3000:3000'
+  depends_on:
+   - 'mongo'
+  environment:
+    - MONGO_INITDB_ROOT_USERNAME:
+    - MONGO_INITDB_ROOT_PASSWORD: 
+    - ME_CONFIG_MONGODB_URL: mongodb://localhost/ecommerce-full-stack
+    - ME_CONFIG_BASICAUTH: false
+
+
+    FROM node:18-alpine
+RUN mkdir -p /home/app
+COPY . /home/app
+COPY package.json ./
+RUN npm install
+EXPOSE 3000
+CMD ["node", "/home/app/index.html"]
+
+*/
