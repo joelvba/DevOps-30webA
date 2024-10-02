d90a82fa5a6d87ef365850084dedc01dcf9962f2
Joel <joelvb1993@gmail.com>
2024-10-01 18:24:48 -0600

    Agragar Layout

--- My-app/ecommerce/pages/_app.js
+++ My-app/ecommerce/pages/_app.js
@@ -1,9 +1,13 @@
 import {ChakraProvider} from '@chakra-ui/react'
+import layout from '../components/layout'
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+    <layout>
     <Component {...pageProps} />
+    </layout>
+   
     </ChakraProvider>
 
 
