96b648021e50e7e9c60b499297079a6fe6abf604
Joel <joelvb1993@gmail.com>
2024-10-01 18:30:58 -0600

    guardar

--- My-app/ecommerce/pages/_app.js
+++ My-app/ecommerce/pages/_app.js
@@ -1,12 +1,12 @@
 import {ChakraProvider} from '@chakra-ui/react'
-import layout from '../components/layout'
+
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-    <layout>
+    
     <Component {...pageProps} />
-    </layout>
+    
    
     </ChakraProvider>
 
