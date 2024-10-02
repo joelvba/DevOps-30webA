a337fc0466cf8bd261b8521ac329914c986b138a
Joel <joelvb1993@gmail.com>
2024-10-01 20:33:00 -0600

    Conexion db

---
+++ My-app/ecommerce/utils/db.js
@@ -0,0 +1,38 @@
+async function connect() {
+    if (connection.isConnected) {
+      console.log("Conexión Exitosa 🚀");
+      return;
+    }
+    if(mongoose.connection.length > 0){
+      connection.isConnected = mongoose.connections[0].readyState
+      if(connection.isConnected === 1)
+        console.log("Usuario ya conectado 💻");
+      return
+    }
+  
+    const db = await mongoose.connect(process.env.MONGODB_URI)
+    console.log("nueva conexión 😃");
+    connection.isConnected = db.connections[0].readyState
+  }
+  
+  async function disconnect() {
+    if(connection.isConnected){
+      if(process.env.NODE_ENV === 'production'){
+          await mongoose.disconnect()
+          connection.isConnected = false
+      } else {
+        console.log("conectado 💻");
+      }
+    }
+  }
+  
+  function convertDocToObj(doc) {
+    doc._id = doc._id.toString() // convierte el valor de la propiedad "_id" a una cadena o string
+    doc.createdAt = doc.createdAt.toString() // convierte el valor de la propiedad "createdAt" a una cadena o string
+    doc.updatedAt = doc.updatedAt.toString() // convierte el valor de la propiedad "updatedAt" a una cadena o string
+    return doc // devuelve el objeto modificado
+  }
+  
+  const db = { connect, disconnect, convertDocToObj }
+  
+  export default db
