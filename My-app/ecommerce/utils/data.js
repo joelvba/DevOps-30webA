73d7e40a7699a16f14d7c1077e915c2297e5328e
Joel <joelvb1993@gmail.com>
2024-10-01 18:06:07 -0600

    base datos laptops

---
+++ My-app/ecommerce/utils/data.js
@@ -0,0 +1,83 @@
+/* Se agrega productos y imagenes*/
+export const data = {
+    produc:[
+        {
+            id:1,
+            title:'Product 1',
+            price:100,
+            descripcion: 'Producto',
+            image:'/Acer.jpg'
+            
+
+        },
+   
+    
+
+        {
+            id:2,
+            title:'Product 2',
+            price:200,
+            descripcion: 'Producto',
+            image:'/Apple.jpg'
+
+        },
+   
+    
+    
+        {
+            id:3,
+            title:'Product 3',
+            price:300,
+            descripcion: 'Producto',
+            image:'/Dell_5620f.avif'
+
+        },
+   
+    
+    
+        {
+            id:4,
+            title:'Product 4',
+            price:400,
+            descripcion: 'Producto',
+            image:'/Dell.jpg'
+
+        },
+
+        {
+            id:5,
+            title:'Product 5',
+            price:500,
+            descripcion: 'Producto',
+            image:'/Gaming_Acer.jpg'
+
+        },
+        {
+            id:6,
+            title:'Product 6',
+            price:600,
+            descripcion: 'Producto',
+            image:'/Lenovo_ideaPad.avif'
+
+        },
+        {
+            id:7,
+            title:'Product 7',
+            price:700,
+            descripcion: 'Producto',
+            image:'/lenovo.avif'
+
+        },
+        {
+            id:8,
+            title:'Product 8',
+            price:800,
+            descripcion: 'Producto',
+            image:'/Samsug.jpg'
+
+        },
+   
+    ]     
+
+}       
+
