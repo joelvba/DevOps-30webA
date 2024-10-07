11d18f3d59b1c7f4f05ee85d2d09749de1e296e7
Joel <joelvb1993@gmail.com>
2024-10-07 08:25:39 -0600

    creacion de insetar y eliminar

---
+++ My-app/ecommerce/pages/api/seed.js
@@ -0,0 +1,25 @@
+import { createRouter } from 'next-connect'
+import db from '../../utils/db'
+import {data} from '../../utils/data'
+import Product from '../../models/Products'
+const router = createRouter()
+
+router
+  .get(async (req, res) => {
+    //? MONGOOSE FUNCTIONS
+    await db.connect()
+    await Product.deleteMany()
+    await Product.insertMany(data.produc)
+    await db.disconnect()
+    res.send('IT WORKS')
+  })
+
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).end(err.message);
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end('Page not found')
+  }
+});
