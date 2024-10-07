ac461873c9276f477462733e9785d7b8af2f4fb3
Joel <joelvb1993@gmail.com>
2024-10-07 14:35:01 -0600

    Obtener productos

--- My-app/ecommerce/pages/index.js
+++ My-app/ecommerce/pages/index.js
@@ -6,6 +6,23 @@ export default function Home() {
   const [loading, setLoading]=useState(false);
   const [error, setError]= useState(false);
 
+  useEffect(()=>{ 
+    fetchProducts()
+  },[])
+
+  async function fetchProducts() {
+    try {
+      setLoading(true)
+      setError(false)
+      const res = await fetch('/api/products')
+      const newProducts = await res.json()
+      setProducts(newProducts)
+    }catch (err) {
+      setError(true)
+    }
+    setLoading(false)
+  }
+
   return (
     <>    
         <Products/>
