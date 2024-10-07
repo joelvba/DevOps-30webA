74e456459dcef7a3c4fd41f12cdc9b8260effc08
Joel <joelvb1993@gmail.com>
2024-10-07 08:24:53 -0600

    creacion conexion mongo

--- My-app/ecommerce/pages/index.js
+++ My-app/ecommerce/pages/index.js
@@ -1,6 +1,11 @@
 import Products from "../components/Product"
+import { useState, useEffect } from "react"
 
 export default function Home() {
+  const [products, setProducts]= useState([]);
+  const [loading, setLoading]=useState(false);
+  const [error, setError]= useState(false);
+
   return (
     <>    
         <Products/>
