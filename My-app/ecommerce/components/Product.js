f1e00a18d1f94ec28b91528cb20b1a4228edd373
Joel <joelvb1993@gmail.com>
2024-10-07 18:22:38 -0600

    Ajuste de imagen

--- My-app/ecommerce/components/Product.js
+++ My-app/ecommerce/components/Product.js
@@ -14,14 +14,22 @@ const Products = () => {
                 <GridItem key={produc.id}>
                     <Card>
                       <Link href={`/product/${produc.id}`}>
-                      
+                        <Box display="flex" justifyContent="center" py={1} px={3}>
+                                                  
                         <Image 
-                            src={`/image${produc.image}`}
-                            height={230}
-                            width={230}
-                            style={{objectFit: "cover" , height:"230px"}}
-                            alt={produc.title}
-                        />
+                                src={`/image${produc.image}`}
+                                height={230}
+                                width={230}
+                                style={{
+                                    width: "230px",      // Ajusta el ancho a 230px
+                                    height: "230px",     // Ajusta la altura a 230px
+                                    objectFit: "fill",   // "fill" estira la imagen para llenar el contenedor, aunque puede deformarse
+                                }}
+                                alt={produc.title}
+                            />
+
+                        </Box>
+
                         <Box py={1} px={3}>
                             <h2>{produc.title}</h2>
                             <div>{produc.descripcion}</div>
