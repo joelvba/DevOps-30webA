823443ee3fe755cdf3b65b526fd97f2e26aca7ae
Joel <joelvb1993@gmail.com>
2024-10-07 18:23:27 -0600

    agregar products

--- My-app/ecommerce/pages/product/[id].js
+++ My-app/ecommerce/pages/product/[id].js
@@ -13,9 +13,11 @@ import {data} from '../../utils/data'
 import { 
     Container, SimpleGrid, Flex, Image, Heading, Stack, Box, Text, 
     useColorModeValue, Button } from '@chakra-ui/react'
+import db from '../../utils/db'
 import Products from '../../components/Product'
+import { AmpContext } from 'next/dist/server/future/route-modules/app-page/vendored/contexts/entrypoints'
 
- const ProductPage = () =>{
+ const ProductPage = ({props}) =>{
 
     const router = useRouter()
     const {id} = router.query
@@ -25,63 +27,77 @@ import Products from '../../components/Product'
         return <div> 404 ! Product not found</div>
     }
 
-  return(
-    <Container maxW={'container.xl'} mt={2}>
-        <SimpleGrid columns={[1,2]} spacing={2}>
-            <Flex>
-                <Image src={`/image/${product.image}`}
-                 rounded={'30px'}
-                 fit={'cover'}
-                 alt={product.description}
-                 align={'center'}
-                 h={'100%'}
-                 w={{base:'100%', sm: '400px', lg: '500px'}}
-                 
-                />
-            </Flex>
-            <Stack spacing={{base:6, md:10}}>
-                <Box>
-                    <Heading
-                     lineHeight={1.3}
-                     fontWeight={600}
-                     fontSize={{base: '2xl', sm:'4xl', lg:'4xl'}}
-                    >{product.title}
-                    </Heading>
-                    <Text
-                    color={useColorModeValue('gray.900', 'gray.400')}
-                    fontWeight={350}
-                    fontSize={'2xl'}
-                    >
-                        {`$${product.price} USD`}
-                    </Text>
-                </Box>
-                    <Text
-                       color={useColorModeValue('gray.500', 'gray.400')}
-                       fontSize={'lg'}
-                       mb={'10'} 
-                    >
-                        {product.descripcion}
-                    </Text>
-                    <Flex flexGrow={1} alignItems={'end'}>
-                        <Button
-                            rounded={'md'} w={'full'} size={'lg'} py={'3'}
-                            bg={useColorModeValue('gray.900', 'gray.50')}
-                            color={useColorModeValue('white', 'gray.900')}
-                            textTransform={'uppercase'}
-                            _hover={{ bg: 'green.400' }}
-                           _focus={{ boxShadow: 'outline' }}
-                           
+    return(
+        <Container maxW={'container.xl'} mt={2}>
+            <SimpleGrid columns={[1,2]} spacing={2}>
+                <Flex>
+                    <Image src={`/image/${product.image}`}
+                    rounded={'30px'}
+                    fit={'cover'}
+                    alt={product.description}
+                    align={'center'}
+                    h={'100%'}
+                    w={{base:'100%', sm: '400px', lg: '500px'}}
+                    
+                    />
+                </Flex>
+                <Stack spacing={{base:6, md:10}}>
+                    <Box>
+                        <Heading
+                        lineHeight={1.3}
+                        fontWeight={600}
+                        fontSize={{base: '2xl', sm:'4xl', lg:'4xl'}}
+                        >{product.title}
+                        </Heading>
+                        <Text
+                        color={useColorModeValue('gray.900', 'gray.400')}
+                        fontWeight={350}
+                        fontSize={'2xl'}
                         >
-                            add to cart
-                        </Button>
-                    </Flex>
+                            {`$${product.price} USD`}
+                        </Text>
+                    </Box>
+                        <Text
+                        color={useColorModeValue('gray.500', 'gray.400')}
+                        fontSize={'lg'}
+                        mb={'10'} 
+                        >
+                            {product.descripcion}
+                        </Text>
+                        <Flex flexGrow={1} alignItems={'end'}>
+                            <Button
+                                rounded={'md'} w={'full'} size={'lg'} py={'3'}
+                                bg={useColorModeValue('gray.900', 'gray.50')}
+                                color={useColorModeValue('white', 'gray.900')}
+                                textTransform={'uppercase'}
+                                _hover={{ bg: 'green.400' }}
+                            _focus={{ boxShadow: 'outline' }}
+                            
+                            >
+                                add to cart
+                            </Button>
+                        </Flex>
 
-            </Stack>
-        </SimpleGrid>
-    </Container>
-    
-  )
+                </Stack>
+            </SimpleGrid>
+        </Container>
+        
+    )
 
- }  
+    }  
+
+    export async function getServerSideProps(){
+        const {params} = context
+        const {id} = params
+        await db.connect()
+        const product = await Products.findOne({id}).lean()
+        await db.disconnect()
+        return{
+           props: {
+            product:db.convertDocToObj(product)
+           }
+        }
+
+    }
  
  export default ProductPage
